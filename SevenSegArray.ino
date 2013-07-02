6ff542fa2eee4e9ab896a49c6b208d5a07f64ca8
Ahmed Saieed <a.saieed@gmail.com>
2013-07-02 19:28:06 +0300

    Refactored pin assignment and setup(). Added license info.

--- SevenSegArray.ino
+++ SevenSegArray.ino
@@ -1,33 +1,54 @@
+/*
+  Multi-digit 7-segment Display Array Controller
+ 
+  Controls N-digit 7-segment display. Both Common Anode and Common Cathode displays supported.
+ 
+ created 2 Jul 2012
+ by A. Saieed - a DOT saieed AT gmail.com
+
+ Licensed under CC-BY-NC-SA 3.0 - http://creativecommons.org/licenses/by-nc-sa/3.0/
+ 
+*/
+
 #include "math.h"
 
+// Tested on Arduino 2560. Might not work with UNO.
 
 void intTo7Seg(int num);
+
 int a,b,c,d,e,f,g;
+
 int number = 12345;
+
 int freq;
+
+// The number of pins for the digit enablers in enable_digit[], implicitly defines the
+// number of 7-segment used.
+
+// Example:
+// enable_digit[] = {1, 2, 3};
+// defines a 3-digit 7-segment whose selector pins are connected to the Arduino at pins 1, 2 and 3.
+
+enable_digit[] = { 3, 4, 5, 6, 7};
 
 const int num_digits = 5;
 
 int enable_digit[num_digits];
 int digits[num_digits+1];
 
+void setPins(int a, int b, int c, int d, int e, int f, int g)
 
-void setup() {
 
-  a = 28;
-  b = 30;
-  c = 26;
-  d = 24;
-  e = 22;
-  f = 32;
-  g = 34;
 
-  enable_digit[0] = 3;
-  enable_digit[1] = 4;
-  enable_digit[2] = 5;
-  enable_digit[3] = 6;
-  enable_digit[4] = 7;
-
+setPins(int a_pin, int b_pin, int c_pin, int d_pin, int e_pin, int f_pin, int g_pin) {
+  a = a_pin;
+  b = b_pin;
+  c = c_pin;
+  d = d_pin;
+  e = e_pin;
+  f = f_pin;
+  g = g_pin;
+  
   pinMode(a, OUTPUT);
   pinMode(b, OUTPUT);
   pinMode(c, OUTPUT);
@@ -41,7 +62,10 @@ void setup() {
   pinMode(enable_digit[2], OUTPUT);
   pinMode(enable_digit[3], OUTPUT);
   pinMode(enable_digit[4], OUTPUT);
+}
 
+void sevseg_off() {
+ // initialize to OFF
   digitalWrite(a, LOW);
   digitalWrite(b, LOW);
   digitalWrite(c, LOW);
@@ -55,8 +79,15 @@ void setup() {
   digitalWrite(enable_digit[2], LOW);
   digitalWrite(enable_digit[3], LOW);
   digitalWrite(enable_digit[4], LOW);
+ 
+}
 
+void setup() {
 
+  // call setpins to set it up.
+  setPins(28, 30, 26, 24, 22, ,32, 34);
+  
+  sevseg_off();
 
 }
 
